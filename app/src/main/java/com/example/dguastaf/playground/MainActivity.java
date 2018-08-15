55e641cfc00cf8139f0dd9684b1ff688e08353fe
Doug Guastaferro <dguastaferro@gmail.com>
2018-08-14 23:31:01 -0700

    Data binding example

--- app/src/main/java/com/example/dguastaf/playground/MainActivity.java
+++ app/src/main/java/com/example/dguastaf/playground/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.dguastaf.playground;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dguastaf.playground.Models.User;
+import com.example.dguastaf.playground.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Doug");
+        binding.setUser(user);
     }
 }
---
+++ app/src/main/java/com/example/dguastaf/playground/Models/User.java
@@ -0,0 +1,13 @@
+package com.example.dguastaf.playground.Models;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
